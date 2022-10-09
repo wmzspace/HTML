50cfea90e6af740efda26d670ce842254d14ce64
HelloWorld <sc21m2w@leeds.ac.uk>
2022-10-09 22:25:49 +0800

    temp

--- temp/temp.js
+++ temp/temp.js
@@ -5,23 +5,17 @@ var div = document.createElement("div");
         figcaption.innerText = "JS style";
         figure.appendChild(figcaption);
         const order = ["st", "nd", "rd", "th"];
-        var figureP = [], smalls = [], sups = [], figureP2 = [];
+        var figureP = [], smalls = [], sups = [];
         for (let i = 1; i <= 5; i++) {
-
             figureP[i - 1] = document.createElement("p");
-            figureP[i - 1].innerText += i;
             smalls[i - 1] = document.createElement("small");
             sups[i - 1] = document.createElement("sup");
+            figureP[i - 1].innerText += i;
             sups[i - 1].innerText = order[i <= 4 ? i - 1 : 3];
             smalls[i - 1].appendChild(sups[i - 1]);
             figureP[i - 1].appendChild(smalls[i - 1]);
-            figureP2[i - 1] = document.createElement("p");
-            figureP2[i - 1].style.display = "inline";
-            figureP2[i - 1].innerText = " Line.";
-            figureP[i - 1].appendChild(figureP2[i - 1]);
             figure_div.appendChild(figureP[i - 1]);
         }
-
         figure.appendChild(figure_div);
         div.appendChild(figure);
         document.body.appendChild(div);
